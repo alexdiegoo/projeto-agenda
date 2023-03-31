c1ff23cd6a610c19ecfabeedaf03a5115861c306
alexdiegoo <alex.diegosn10@gmail.com>
2023-03-31 19:10:48 -0300

    verificação de usuario ja existente

--- src/models/loginModel.js
+++ src/models/loginModel.js
@@ -26,6 +26,9 @@ class Login {
         this.valida();
         if(this.errors.length > 0) return;
 
+        await this.userExists();
+        if(this.errors.length > 0) return;
+
         try {
             const salt = bcryptjs.genSaltSync();
             this.body.password = bcryptjs.hashSync(this.body.password, salt);
@@ -34,6 +37,11 @@ class Login {
         } catch(err) {
             console.log(err);
         }
+    }
+
+    async userExists() {
+        const user = await LoginModel.findOne({ email: this.body.email });
+        if(user) this.errors.push("Usuario ja existe");
     }
 
     valida() {
